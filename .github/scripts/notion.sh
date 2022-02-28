ac1f21b3a3ad1a56c1248b6c2b0c46796303a443
Taylor Dawson <taylorjdawson@gmail.com>
2022-02-28 11:05:59 -0800

    Update notion.sh

--- .github/scripts/notion.sh
+++ .github/scripts/notion.sh
@@ -1,1 +1,5 @@
 echo '${{ toJSON(github) }}'
+curl 'https://api.notion.com/v1/pages' \
+  -H 'Authorization: Bearer '"$NOTION_API_KEY"'' \
+  -H "Content-Type: application/json" \
+  -H "Notion-Version: 2021-08-16" \
