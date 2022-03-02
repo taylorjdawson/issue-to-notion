803d2ddad2f15f85f8857460d7602b1bef1ac963
Taylor Dawson <taylorjdawson@gmail.com>
2022-03-02 14:27:13 -0800

    Update script

--- .github/scripts/notion.sh
+++
@@ -1,6 +0,0 @@
-curl 'https://api.notion.com/v1/pages' \
-  -H 'Authorization: Bearer '"$NOTION_TOKEN"'' \
-  -H "Content-Type: application/json" \
-  -H "Notion-Version: 2021-08-16" \
-  --data '{"parent":{"type":"database_id","database_id":"29876f9a9b864ca39a984f42e17fd345"},"icon":{"type":"emoji","emoji":"🐛"},"properties":{"Name":{"type":"title","title":[{"type":"text","text":{"content":"'"$TITLE"'"}}]},"Assigned":{"people":[{"object":"user","id":"f6cbb362-4908-4138-9ef0-434003d9a9f8"}]},"Status":{"select":{"name":"Current Sprint"}},"Type":{"multi_select":[{"name":"bug"}]}}}'
-
